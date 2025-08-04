01a92f924aab1c09ccd063408a2078971764a47a
huterguier <huterguier@gmail.com>
2025-08-04 19:32:54 +0200

    implemented callbacks

--- gxm/wrappers/gymnasium.py
+++ gxm/wrappers/gymnasium.py
@@ -3,6 +3,7 @@ from typing import Any
 
 import gymnasium
 import jax
+import jax.numpy as jnp
 
 from gxm.core import Env, EnvState, State
 
@@ -16,52 +17,89 @@ class GymnasiumState(State):
 
 
 class GymnasiumEnv(Env):
-    obs_shape_dtype: Any
+    id: str
+    env_state_shape_dtype: Any
     n_actions: int
 
     def __init__(self, id: str, **kwargs):
-        env = gymnasium.make(id, **kwargs)
+        self.id = id
+        env = gymnasium.make(self.id, **kwargs)
         obs, _ = env.reset()
-        obs = jax.numpy.array(obs)
-        n_actionss = env.action_space.n
-        self.obs_shape_dtype = jax.ShapeDtypeStruct(obs.shape, obs.dtype)
+        obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        env_state = EnvState(
+            state=GymnasiumState(time=jnp.int32(0), id=jnp.int32(0)),
+            obs=jnp.array(obs),
+            reward=jnp.array(reward),
+            done=jnp.array(terminated or truncated),
+            info=info,
+        )
+        self.env_state_shape_dtype = jax.tree.map(
+            lambda x: jax.ShapeDtypeStruct(x.shape, x.dtype), env_state
+        )
+        n_actions = env.action_space.n
 
     def step(
         self, key: jax.Array, state: EnvState | GymnasiumState, action: jax.Array
     ) -> EnvState:
+        if isinstance(state, EnvState):
+            env_state = state
+            state = env_state.state
 
         def callback(key: jax.Array, state: GymnasiumState, action: jax.Array):
-            env = gymnasium.make("CartPole-v1")
-            obs, reward, terminated, truncated, info = env.step(action)
+            env = envs.get(str(state.id), None)
+            import numpy as np
+
+            obs, reward, terminated, truncated, info = env.step(np.array(action))
             obs = jax.numpy.array(obs)
-            return EnvState(
+            print(state)
+            env_state = EnvState(
                 state=state,
-                obs=obs,
-                reward=reward,
-                done=done,
+                obs=jnp.array(obs),
+                reward=jnp.array(reward),
+                done=jnp.array(terminated or truncated),
                 info=info,
             )
+            return env_state
 
         env_state = jax.pure_callback(
             callback,
-            self.obs_shape_dtype,
-            key=key,
-            state=state,
-            action=action,
-            name="gymnasium_step",
-            vmap_method=
+            self.env_state_shape_dtype,
+            key,
+            state,
+            action,
+            vmap_method="broadcast_all",
         )
 
+        return env_state
 
-        pass
+    def reset(self, key: jax.Array) -> EnvState:
+        def callback(key: jax.Array):
+            env = gymnasium.make("CartPole-v1")
+            env_id = 0
+            envs[str(env_id)] = env
+            obs, info = env.reset()
+            obs = jnp.array(obs)
+            env_state = EnvState(
+                state=GymnasiumState(time=jnp.int32(0), id=jnp.int32(env_id)),
+                obs=jnp.array(obs),
+                reward=jnp.array(0.0),
+                done=jnp.array(False),
+                info=info,
+            )
+            return env_state
 
-    def reset(key: jax.Array) -> EnvState:
-        def callback():
-
-        pass
+        env_state = jax.pure_callback(
+            callback,
+            self.env_state_shape_dtype,
+            key,
+            vmap_method="broadcast_all",
+        )
+        return env_state
 
     def num_actions():
-        return 2
+        return self.n_actions
 
 
 env = GymnasiumEnv("CartPole-v1")
+env_state = env.reset(jax.random.key(0))
+env_state = env.step(jax.random.key(0), env_state, jax.numpy.array(0))
