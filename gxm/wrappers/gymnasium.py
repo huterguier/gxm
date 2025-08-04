397941f1baf66f7411dbccd3e423b2a67bfb7b72
huterguier <huterguier@gmail.com>
2025-08-04 13:29:26 +0200

    gymnasium wrapper init

---
+++ gxm/wrappers/gymnasium.py
@@ -0,0 +1,23 @@
+import gymnasium
+import jax
+
+from gxm.core import Env
+
+envs = {}
+
+
+class GymnasiumEnv(Env):
+
+    def __init__(id: str, **kwargs):
+
+
+    def step():
+
+
+    def reset():
+
+
+    def num_actions():
+
+
+
