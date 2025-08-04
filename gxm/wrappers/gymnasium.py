797feb90583632050c2fb24597c7817c850fc30a
huterguier <huterguier@gmail.com>
2025-08-04 23:09:09 +0200

    added benchmark

--- gxm/wrappers/gymnasium.py
+++ gxm/wrappers/gymnasium.py
@@ -8,6 +8,7 @@ import jax.numpy as jnp
 from gxm.core import Env, EnvState, State
 
 envs = {}
+current_env_id = 0
 
 
 @jax.tree_util.register_dataclass
@@ -36,7 +37,7 @@ class GymnasiumEnv(Env):
         self.env_state_shape_dtype = jax.tree.map(
             lambda x: jax.ShapeDtypeStruct(x.shape, x.dtype), env_state
         )
-        n_actions = env.action_space.n
+        self.n_actions = env.action_space.n
 
     def step(
         self, key: jax.Array, state: EnvState | GymnasiumState, action: jax.Array
@@ -51,7 +52,6 @@ class GymnasiumEnv(Env):
 
             obs, reward, terminated, truncated, info = env.step(np.array(action))
             obs = jax.numpy.array(obs)
-            print(state)
             env_state = EnvState(
                 state=state,
                 obs=jnp.array(obs),
@@ -75,7 +75,11 @@ class GymnasiumEnv(Env):
     def reset(self, key: jax.Array) -> EnvState:
         def callback(key: jax.Array):
             env = gymnasium.make("CartPole-v1")
-            env_id = 0
+            env = gymnasium.wrappers.Autoreset(env)
+            global current_env_id
+            env_id = current_env_id
+            current_env_id += 1
+
             envs[str(env_id)] = env
             obs, info = env.reset()
             obs = jnp.array(obs)
@@ -96,10 +100,71 @@ class GymnasiumEnv(Env):
         )
         return env_state
 
-    def num_actions():
+    @property
+    def num_actions(self):
         return self.n_actions
 
 
 env = GymnasiumEnv("CartPole-v1")
 env_state = env.reset(jax.random.key(0))
 env_state = env.step(jax.random.key(0), env_state, jax.numpy.array(0))
+
+n_steps = 10000
+key = jax.random.key(0)
+
+
+@jax.jit
+def f(x, key):
+    key, subkey = jax.random.split(key)
+    x += jax.random.normal(subkey, x.shape)
+    return x, key
+
+
+import numpy as np
+
+
+@jax.jit
+def f(x, key):
+    key, subkey = jax.random.split(key)
+    x += jax.random.normal(subkey, x.shape)
+    return x, key
+
+
+def rollout_for(key):
+    env = gymnasium.make("CartPole-v1")
+    env = gymnasium.wrappers.Autoreset(env)
+    obs, info = env.reset()
+    x = jnp.array(0.0)
+    for i in range(n_steps):
+        obs, reward, termination, trunction, info = env.step(
+            np.array(jax.random.randint(key, (), 0, env.action_space.n))
+        )
+        x, key = f(x, key)
+    return obs
+
+
+@jax.jit
+def rollout_scan(key):
+    def step(env_state, key):
+        action = jax.random.randint(key, (), 0, env.num_actions)
+        return env.step(key, env_state, action), None
+
+    env_state = env.reset(key)
+    keys = jax.random.split(key, n_steps)
+    env_state, _ = jax.lax.scan(step, env_state, keys)
+    return env_state.obs
+
+
+import time
+
+start = time.time()
+obs_for = rollout_for(key)
+print("Time for rollout_for:", time.time() - start)
+
+start = time.time()
+obs_scan = rollout_scan(key)
+print("Time for rollout_scan:", time.time() - start)
+
+start = time.time()
+obs_scan = rollout_scan(key)
+print("Time for rollout_scan again:", time.time() - start)
