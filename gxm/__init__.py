f1a43f6dc936bd122348be7027fe343aa7452681
huterguier <huterguier@gmail.com>
2025-09-08 11:33:19 +0200

    fixed init

--- gxm/__init__.py
+++ gxm/__init__.py
@@ -1,14 +1,10 @@
 from gxm.core import Environment, EnvironmentState, Timestep, Trajectory
 from gxm.registration import make
 
-from . import environments, wrappers
-
 __all__ = [
     "Environment",
     "EnvironmentState",
     "Timestep",
     "Trajectory",
     "make",
-    "environments",
-    "wrappers",
 ]
