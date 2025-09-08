2c2eb8916a13f9623035e694a8693d9f84d0b43c
huterguier <huterguier@gmail.com>
2025-09-08 11:39:02 +0200

    fixed circular import

--- gxm/environments/__init__.py
+++ gxm/environments/__init__.py
@@ -1,6 +1,6 @@
-from gxm.environments.envpool import EnvpoolEnvironment
-from gxm.environments.gymnax import GymnaxEnvironment
-from gxm.environments.pgx import PgxEnvironment
+from gxm.environments.envpool_environment import EnvpoolEnvironment
+from gxm.environments.gymnax_environment import GymnaxEnvironment
+from gxm.environments.pgx_environment import PgxEnvironment
 
 __all__ = [
     "EnvpoolEnvironment",
