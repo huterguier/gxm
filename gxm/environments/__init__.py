163d7fb2171b0f719c6ab45ced20afd672cbb841
Noah Farr <login@noahfarr.com>
2025-09-11 21:42:45 +0200

    Add gymnasium env

--- gxm/environments/__init__.py
+++ gxm/environments/__init__.py
@@ -2,10 +2,12 @@ from gxm.environments.craftax_environment import CraftaxEnvironment
 from gxm.environments.envpool_environment import EnvpoolEnvironment
 from gxm.environments.gymnax_environment import GymnaxEnvironment
 from gxm.environments.pgx_environment import PgxEnvironment
+from gxm.environments.gymnasium_environment import GymnasiumEnvironment
 
 __all__ = [
     "EnvpoolEnvironment",
     "GymnaxEnvironment",
     "PgxEnvironment",
     "CraftaxEnvironment",
+    "GymnasiumEnvironment",
 ]
