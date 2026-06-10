22ae6545a5ed7d284d2e57da0b80a46ce0af6426
huterguier <huterguier@gmail.com>
2026-06-10 15:42:15 +0200

    Fix typing of gymnax params

--- gxm/adapters/gymnax.py
+++ gxm/adapters/gymnax.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any
 
 import gymnax
 import gymnax.environments.spaces as gymnax_spaces
@@ -15,9 +15,13 @@ def _gymnax_to_gxm_space(gymnax_space) -> Space:
     if isinstance(gymnax_space, gymnax_spaces.Discrete):
         return Discrete(gymnax_space.n)
     if isinstance(gymnax_space, gymnax_spaces.Box):
-        return Box(low=gymnax_space.low, high=gymnax_space.high, shape=gymnax_space.shape)
+        return Box(
+            low=gymnax_space.low, high=gymnax_space.high, shape=gymnax_space.shape
+        )
     if isinstance(gymnax_space, gymnax_spaces.Dict):
-        return Tree({k: _gymnax_to_gxm_space(v) for k, v in gymnax_space.spaces.items()})
+        return Tree(
+            {k: _gymnax_to_gxm_space(v) for k, v in gymnax_space.spaces.items()}
+        )
     if isinstance(gymnax_space, gymnax_spaces.Tuple):
         return Tree([_gymnax_to_gxm_space(s) for s in gymnax_space.spaces])
     raise NotImplementedError(f"Gymnax space type {type(gymnax_space)} not supported.")
@@ -32,7 +36,9 @@ def _gxm_to_gymnax_space(space: Space):
         if isinstance(space.spaces, (list, tuple)):
             return gymnax_spaces.Tuple([_gxm_to_gymnax_space(s) for s in space.spaces])
         if isinstance(space.spaces, dict):
-            return gymnax_spaces.Dict({k: _gxm_to_gymnax_space(v) for k, v in space.spaces.items()})
+            return gymnax_spaces.Dict(
+                {k: _gxm_to_gymnax_space(v) for k, v in space.spaces.items()}
+            )
     raise NotImplementedError(f"Gxm space type {type(space)} not supported.")
 
 
@@ -52,7 +58,9 @@ class GymnaxAdapter(Environment[GymnaxState]):
         self.id = f"Gymnax/{id}"
         self.env, self.env_params = gymnax.make(id, **kwargs)
         self.action_space = _gymnax_to_gxm_space(self.env.action_space(self.env_params))
-        self.observation_space = _gymnax_to_gxm_space(self.env.observation_space(self.env_params))
+        self.observation_space = _gymnax_to_gxm_space(
+            self.env.observation_space(self.env_params)
+        )
 
     def init(self, key: Key) -> tuple[GymnaxState, Timestep]:
         obs, gymnax_state = self.env.reset(key, self.env_params)
@@ -72,9 +80,13 @@ class GymnaxAdapter(Environment[GymnaxState]):
         del env_state
         return self.init(key)
 
-    def step(self, key: Key, env_state: GymnaxState, action: Array) -> tuple[GymnaxState, Timestep]:
+    def step(
+        self, key: Key, env_state: GymnaxState, action: Array
+    ) -> tuple[GymnaxState, Timestep]:
         gymnax_state = env_state.gymnax_state
-        obs, gymnax_state, reward, done, _ = self.env.step(key, gymnax_state, action, self.env_params)
+        obs, gymnax_state, reward, done, _ = self.env.step(
+            key, gymnax_state, action, self.env_params
+        )
         env_state = GymnaxState(gymnax_state=gymnax_state)
         timestep = Timestep(
             next_obs=obs,
@@ -100,7 +112,9 @@ class _GymnaxToGxm(Environment[_WrappedGymnaxState]):
         self._params = params if params is not None else env.default_params
         self.id = "gymnax_wrapped"
         self.action_space = _gymnax_to_gxm_space(self._env.action_space(self._params))
-        self.observation_space = _gymnax_to_gxm_space(self._env.observation_space(self._params))
+        self.observation_space = _gymnax_to_gxm_space(
+            self._env.observation_space(self._params)
+        )
 
     def init(self, key: Key) -> tuple[_WrappedGymnaxState, Timestep]:
         obs, state = self._env.reset(key, self._params)
@@ -117,11 +131,15 @@ class _GymnaxToGxm(Environment[_WrappedGymnaxState]):
         )
         return _WrappedGymnaxState(gymnax_state=state), timestep
 
-    def reset(self, key: Key, env_state: _WrappedGymnaxState) -> tuple[_WrappedGymnaxState, Timestep]:
+    def reset(
+        self, key: Key, env_state: _WrappedGymnaxState
+    ) -> tuple[_WrappedGymnaxState, Timestep]:
         del env_state
         return self.init(key)
 
-    def step(self, key: Key, env_state: _WrappedGymnaxState, action: Array) -> tuple[_WrappedGymnaxState, Timestep]:
+    def step(
+        self, key: Key, env_state: _WrappedGymnaxState, action: Array
+    ) -> tuple[_WrappedGymnaxState, Timestep]:
         obs, state, reward, done, info = self._env.step(
             key, env_state.gymnax_state, action, self._params
         )
@@ -145,21 +163,27 @@ class _GxmToGymnax:
     def default_params(self) -> None:
         return None
 
-    def step(self, key: Key, state: Any, action: Any, params: Optional[Any] = None):
+    def step(self, key: Key, state: Any, action: Any, params: Any | None = None):
         del params
         next_state, timestep = self._env.step(key, state, action)
-        return timestep.next_obs, next_state, timestep.reward, timestep.done, timestep.info
+        return (
+            timestep.next_obs,
+            next_state,
+            timestep.reward,
+            timestep.done,
+            timestep.info,
+        )
 
-    def reset(self, key: Key, params: Optional[Any] = None):
+    def reset(self, key: Key, params: Any | None = None):
         del params
         state, timestep = self._env.init(key)
         return timestep.next_obs, state
 
-    def action_space(self, params: Optional[Any] = None):
+    def action_space(self, params: Any | None = None):
         del params
         return _gxm_to_gymnax_space(self._env.action_space)
 
-    def observation_space(self, params: Optional[Any] = None):
+    def observation_space(self, params: Any | None = None):
         del params
         return _gxm_to_gymnax_space(self._env.observation_space)
 
